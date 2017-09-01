e25cbb369a903755ce994fce269bbbe4eba0598e
Adam Farmer <adam@Adams-MacBook-Air.local>
2017-09-01 14:45:43 -0700

    blocks can stack

--- public/canvas.js
+++ public/canvas.js
@@ -8,15 +8,17 @@ var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 "T", "U", "V", "W", "X", "Y", "Z"];
 var letters = [];
 var i = 0;
+var randyCounter = 4;
 var bgReady = false;
 var letterReady = false;
 var bgImage = new Image();
 var xCor = [10, 130, 250, 370];
-var Letter = function(){
-    this.randy = Math.floor(Math.random()* 4);
+var randy = 2;
+var Letter = function()
+{
     this.random = Math.floor(Math.random()* 26);
     this.value = alphabet[this.random];
-    this.x = xCor[this.randy];
+    this.x = xCor[randy];
     this.y = -100;
     this.image = new Image();
 }
@@ -43,10 +45,60 @@ bgImage.onload = function() {
 };
 bgImage.src = "assets/images/bg.jpg";
 
+//looks for stacking
+function colFilled(col)
+{
+    var counter = 0;
+    for (var i = 0; i < letters.length-1; i++)
+    {
+      if(letters[i].x === col)
+      {
+        counter++;
+      }
+    }
+    //console.log(counter);
+    switch (counter)
+    {
+      case 0:
+        return 742;
+        break;
+      case 1:
+        return 632;
+        break;
+      case 2:
+        return 522;
+        break;
+      case 3:
+        return 412;
+        break;
+      case 4:
+        return 800;
+        break;
+    }
+}
 
+function randyFunk()
+{
+  randy = Math.floor(Math.random()*randyCounter);
+  if(randyCounter === 0)
+  {
+    return;
+  }
+
+  for (var i = 0; i < xCor.length; i++)
+  {
+    if(colFilled(xCor[i]) === 800)
+    {
+      xCor.splice(i,1);
+      console.log(i, xCor);
+      randyCounter--;
+    }
+  }
+}
 
 // The main game loop
 var main = function () {
+
 // var now = Date.now();
 // var delta = now - then;
 	render();
@@ -56,17 +108,17 @@ var main = function () {
 	requestAnimationFrame(main);
 };
 
-var update = function() {
-    if (letter.y < 742) {
-        letter.y += 1;
+var update = function()
+{
+    if (letter.y < colFilled(letter.x)) {
+        letter.y += 5;
         if (keysDownCurrent[37] && !keysDownPrevious[37] && letter.x > 10) {
             letter.x -= 120;
         }
-
         if (keysDownCurrent[39] && !keysDownPrevious[39] && letter.x < 370) {
             letter.x += 120;
         }
-        if(letter.y >= 742)
+        if(letter.y >= colFilled(letter.x))
         {
             letterReady = false;
             i++;
@@ -81,17 +133,15 @@ var render = function () {
 	if (bgReady) {
 		ctx.drawImage(bgImage, 0, 0);
 	}
-for (var i = 0; i < letters.length; i++)
-{
-  if (letterReady)
+  for (var i = 0; i < letters.length; i++)
   {
       ctx.drawImage(letters[i].image, letters[i].x , letters[i].y);
   }
 }
 
-};
-
 function loop() {
+  randyFunk();
+  //need to figure out randyFunk...
     if(i === 16) {
         return;
     }

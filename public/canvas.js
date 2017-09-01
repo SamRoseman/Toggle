387d09ccf5a3313330419a5b12673f04d6fb929c
Samantha Roseman <samantharoseman@Sams-MacBook-Pro.local>
2017-09-01 13:15:11 -0700

    spoopy letters

--- public/canvas.js
+++ public/canvas.js
@@ -6,7 +6,6 @@ canvas.height = 852;
 document.body.appendChild(canvas);
 var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
 "T", "U", "V", "W", "X", "Y", "Z"];
-var letters = [];
 var i = 0;
 var bgReady = false;
 var letterReady = false;
@@ -81,14 +80,10 @@ var render = function () {
 	if (bgReady) {
 		ctx.drawImage(bgImage, 0, 0);
 	}
-for (var i = 0; i < letters.length; i++)
-{
-  if (letterReady)
-  {
-      ctx.drawImage(letters[i].image, letters[i].x , letters[i].y);
-  }
-}
 
+    if (letterReady) {
+        ctx.drawImage(letter.image, letter.x , letter.y);
+    }
 };
 
 function loop() {
@@ -97,7 +92,6 @@ function loop() {
     }
 
     letter = new Letter();
-    letters.push(letter);
 
     letter.image.onload = function() {
         letterReady = true;
